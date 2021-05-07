acdbde747c81f09a494c810d6de6efeac00c95e0
BoringBoredom <BoringBoredom>
2021-05-07 15:26:00 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,38 +1,32 @@
 const refreshRateE = document.getElementById('refresh-rate')
-const minFpsE = document.getElementById('min-fps')
-const maxFpsE = document.getElementById('max-fps')
+const maxFpsE = document.getElementById('limit')
 const result = document.getElementById('result')
 
 function calculateCap() {
     const refreshRate = parseInt(refreshRateE.value)
-    const minFps = parseInt(minFpsE.value)
     const maxFps = parseInt(maxFpsE.value)
-
-    if (isNaN(minFps) || isNaN(maxFps) || isNaN(refreshRate) || refreshRate <= 0 || maxFps <= 0 || minFps < 0 || maxFps < minFps) {
+    if (isNaN(maxFps) || isNaN(refreshRate) || refreshRate < 1 || maxFps < 1) {
         result.innerHTML = ''
         return
     }
-
     let viableFpsCaps = []
     let multiplier = 1
     let divider = 1
     let cap
-
     while (true) {
         cap = refreshRate * multiplier
         if (cap > maxFps) {
             break
         }
-        if (cap >= minFps) {
+        if (cap >= 1) {
             viableFpsCaps.push(cap)
         }
         multiplier += 1
     }
-
     while (true) {
         if (refreshRate %divider == 0) {
             cap = refreshRate / divider
-            if (cap < minFps) {
+            if (cap < 1) {
                 break
             }
             if (cap <= maxFps) {
@@ -44,30 +38,22 @@ function calculateCap() {
         }
         divider += 1
     }
-
     viableFpsCaps.sort((a, b) => a - b)
     const sortedViableFpsCaps = [...new Set(viableFpsCaps)]
-
     let caps = ''
     for (const fpsCap of sortedViableFpsCaps) {
         caps += fpsCap.toString() + ', '
     }
-    caps = caps.slice(0, caps.length - 2)
+    caps = caps.slice(0, -2)
     result.innerHTML = caps
 }
 
 refreshRateE.addEventListener('input', () => {
     calculateCap()
 })
-
-minFpsE.addEventListener('input', () => {
-    calculateCap()
-})
-
 maxFpsE.addEventListener('input', () => {
     calculateCap()
 })
-
 window.addEventListener('load', () => {
     calculateCap()
 })
